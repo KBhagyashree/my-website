49f462505b01b8f58e7ef3ab5ba926f87425e30b
KBhagyashree <bhagyashreekhairnar13@gmail.com>
2023-07-28 21:59:20 +0530

    timer creation complete

--- timer.js
+++ timer.js
@@ -9,8 +9,10 @@ let hours = 0;
 let mins = 0;
 let secs = 0;
 let intervalId = 0;
+let timeoutId = 0;
 let timerStartFlag = 1;
 let totalTimeMSecs = 0;
+let alarm = new Audio("IphoneRadarSound.mp3");
 initTimer();
 
 function initTimer(){
@@ -21,60 +23,99 @@ function initTimer(){
             let min = parseInt(e.target.min);
             let max = parseInt(e.target.max);
             let value = parseInt(e.target.value);
-            if (isNaN(value) || value < min)
+            if (isNaN(value) || value < min){
                 e.target.value = e.target.min;
-            else if (value > max)
-                e.target.value = e.target.max;    
+                value = min;
+            }                
+            else if (value > max){
+                e.target.value = e.target.max;
+                value = max;
+            }                    
             else if (value >= 0 && value <=9)
                 e.target.value = "0" + value; 
             else
                 e.target.value = value;     
-            if (input.id == "hours")
-                hours = e.target.value;
-            else if(input.id == "mins")
-                mins = e.target.value;
-            else if(input.id == "secs")
-                secs = e.target.value;
+            if (e.target.id == "hours")
+                hours = value;
+            else if(e.target.id == "mins")
+                mins = value;
+            else if(e.target.id == "secs")
+                secs = value;
         });
     }
 }
 
-function reset() {
+function reset() { 
     for (input of timerValue){
         input.readOnly = false;
         input.value = "00";
-        hours = 0;
-        mins = 0;
-        secs = 0;
     }
+    hours = 0;
+    mins = 0;
+    secs = 0;
+    timerStartFlag = 1;
+    startStopBtn.innerText = "Start";
+    if (intervalId)
+        clearInterval(intervalId);
+    if (timeoutId)
+        clearTimeout(timeoutId);
 }
 
 resetBtn.addEventListener("click", (e)=>{
     reset();
 });
 
-startStopBtn.addEventListener((e) => {
+startStopBtn.addEventListener("click", (e) => {
     thours.readOnly = true;
     tmins.readOnly = true;
     tsecs.readOnly = true;
     totalTimeMSecs = (hours * 3600 + mins * 60 + secs) * 1000;
-    e.target.innerText = "Stop";
+    console.log(hours + ":" + mins + ":" + secs);   
+    console.log(totalTimeMSecs); 
 
     if (timerStartFlag) {
         timerStartFlag = 0;
+        e.target.innerText = "Stop";
+
         intervalId = setInterval(() => {
-            secs--;
-            tsecs.value = secs;
+            if(secs > 0)
+                secs--;
+            else if(secs == 0 && mins > 0){
+                mins--;
+                secs = 59;
+            }
+            else if(mins == 0 && hours > 0){
+                hours--;
+                mins = 59;
+                secs = 59;
+            }
+            // timer countdown display
+            if (secs >= 0 && secs <=9)
+                tsecs.value = "0" + secs;
+            else
+                tsecs.value = secs;
+
+            if (mins >= 0 && mins <=9)
+                tmins.value = "0" + mins;
+            else
+                tmins.value = mins;
+
+            if (hours >= 0 && hours <=9)
+                thours.value = "0" + hours;
+            else
+                thours.value = hours;
+            
         }, 1000);
-        setTimeout(() => {
-            clearInterval(intervalId);
-            e.target.innerText = "Start";
-            reset();
+        timeoutId = setTimeout(() => {
+            alarm.play();
+            reset();            
         }, totalTimeMSecs);
     }
     else{
         timerStartFlag = 1;
+        startStopBtn.innerText = "Start";
         clearInterval(intervalId);
+        clearTimeout(timeoutId);
     }    
 });
 
